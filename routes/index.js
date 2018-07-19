19e624b7ddd74c2154f9400072fa65551bb8d6d1
Maddila <vidya.maddila@honeywell.com>
2018-07-19 18:41:11 +0530

    get and post

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var myParser = require("body-parser");
+var app = express();
+var data;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sagar' });
+  res.render('index', { title: 'PPE-Score' });
+});
+
+/* POST Data */
+router.post("/data", function(request, response) {
+  console.log(request.body);  
+  data = request.get('Hello');
+});
+
+router.get('/view', function(req, res, next) {
+  res.render('view', { title: 'Data Received', name: 'Sagar'});
+ //console.log("view method");
 });
 
 module.exports = router;
